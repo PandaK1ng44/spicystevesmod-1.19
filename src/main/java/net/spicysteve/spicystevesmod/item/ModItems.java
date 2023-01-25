1e70c8034fc12ffb2c81369445d0b9660ecf6320
PandaK1ng44 <crazygamerdude26@gmail.com>
2023-01-26 11:24:40 +1300

    help

--- src/main/java/net/spicysteve/spicystevesmod/item/ModItems.java
+++ src/main/java/net/spicysteve/spicystevesmod/item/ModItems.java
@@ -1,15 +1,20 @@
 package net.spicysteve.spicystevesmod.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.spicysteve.spicystevesmod.SpicyStevesMod;
 
 public class ModItems {
 
+    public static final Item RAW_SPICE = registerItem("raw_spice",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
 
-    private  static Item registerItem(String name , Item item) {
+
+    private static Item registerItem(String name , Item item) {
         return Registry.register(Registry.ITEM, new Identifier(SpicyStevesMod.MOD_ID, name), item);
     }
 
